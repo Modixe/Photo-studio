8d1942e7b6e6fb677676cacebb90410f9b81f9d4
Modixe <Aleksey_97@inbox.ru>
2018-11-22 15:08:50 +0700

    Add files via upload

---
+++ Photo studio/js/scripts.js
@@ -0,0 +1,23 @@
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,2000);
+
+var next = document.getElementById('next');
+var previous = document.getElementById('previous');
+next.onclick = function() {
+  nextSlide();
+};
+previous.onclick = function() {
+  previousSlide();
+};
+function nextSlide() {
+  goToSlide(currentSlide+1);
+}
+function previousSlide() {
+  goToSlide(currentSlide-1);
+}
+function goToSlide(n) {
+  slides[currentSlide].className = 'slide';
+  currentSlide = (n+slides.length)%slides.length;
+  slides[currentSlide].className = 'slide showing';
+}
